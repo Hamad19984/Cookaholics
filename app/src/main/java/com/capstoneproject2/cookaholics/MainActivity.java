63170f9e46280e4fa5425b32d89f5e04c51be088
Hamad19984 <h.amad1998@hotmail.com>
2022-05-29 17:04:51 +0400

    Initial commit

---
+++ app/src/main/java/com/capstoneproject2/cookaholics/MainActivity.java
@@ -0,0 +1,18 @@
+package com.capstoneproject2.cookaholics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
+
